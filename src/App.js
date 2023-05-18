c4d89b24e68cdb444a19cecadd5a23868757347d
yunbiyomi <yunbiyomi@naver.com>
2023-05-18 18:05:56 +0900

    20230518

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useRef} from "react";
 // import Hello from "./Hello";
 // import Wrapper from './Wrapper';
 // import Counter from './Counter';
@@ -6,9 +6,30 @@ import React from "react";
 import UserList from "./UserList";
 
 function App() {
-  return (
-    <UserList />
-  );
+  const users = [
+    {
+        id: 1,
+        username: 'velopert',
+        email: 'public.velopert@gmail.com'
+    },
+    {
+        id: 2,
+        username: 'tester',
+        email: 'tester@example.com'
+    },
+    {
+        id: 3,
+        username: 'liz',
+        email: 'liz@example.com'
+    }
+  ];
+
+  const nextId = useRef(4);
+  const onCreate = () => {
+    nextId.current+=1;
+  };
+
+  return <UserList users={users} />;
 }
 
 export default App;
