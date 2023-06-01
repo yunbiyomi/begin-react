1cc69892cb00a2d11eff6c29f3447d4e0cfb3a14
yunbiyomi <yunbiyomi@naver.com>
2023-06-02 00:56:31 +0900

    styled-components 쌩기초

--- src/App.js
+++ src/App.js
@@ -1,35 +1,53 @@
 import React, {useRef} from "react";
+import styled from 'styled-components';
 // import Hello from "./Hello";
 // import Wrapper from './Wrapper';
 // import Counter from './Counter';
 // import InputSample from "./InputSample";
 import UserList from "./UserList";
 
+
+const SimpleButton = styled.button`
+  color:white;
+  background-color: green;
+`;
+
+const LargeButton = styled(SimpleButton)`
+  font-size: 50px;
+`;
+
+// 이런식으로 하면 적용 안됨
+// const ReactButton = props => {
+//   return <button>{props.children}</button>
+// }
+
+// className을 지정해주어야 적용됨
+const ReactButton = props => {
+  return <button className={props.className}>{props.children}</button>
+}
+
+const ReactLargeButton = styled(ReactButton)`
+  font-size: 50px;
+`;
+
+const PrimaryButton = styled.button`
+  color:${props=>props.primary?'white':'black'};
+  background-color:${props=>props.primary?'black':'white'};
+`;
+
+// 디자인 동적으로 변경
+
 function App() {
-  const users = [
-    {
-        id: 1,
-        username: 'velopert',
-        email: 'public.velopert@gmail.com'
-    },
-    {
-        id: 2,
-        username: 'tester',
-        email: 'tester@example.com'
-    },
-    {
-        id: 3,
-        username: 'liz',
-        email: 'liz@example.com'
-    }
-  ];
-
-  const nextId = useRef(4);
-  const onCreate = () => {
-    nextId.current+=1;
-  };
-
-  return <UserList users={users} />;
+  return (
+    <div>
+      <SimpleButton>Simple</SimpleButton>
+      <LargeButton>Large</LargeButton>
+      <ReactButton>React</ReactButton>
+      <ReactLargeButton>React Large</ReactLargeButton>
+      <PrimaryButton>Normal</PrimaryButton>
+      <PrimaryButton primary>Primary</PrimaryButton>
+    </div>
+  )
 }
 
 export default App;
