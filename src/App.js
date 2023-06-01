18ce03e69cc6cc837617278e88306ca00eec99a3
yunbiyomi <yunbiyomi@naver.com>
2023-06-02 02:06:23 +0900

    styled-components button

--- src/App.js
+++ src/App.js
@@ -1,33 +1,77 @@
-import React, {useRef} from "react";
-import styled, {css} from 'styled-components';
+import React from "react";
+import styled, {ThemeProvider} from 'styled-components';
+import Button from './components/Button';
 // import Hello from "./Hello";
 // import Wrapper from './Wrapper';
 // import Counter from './Counter';
 // import InputSample from "./InputSample";
-import UserList from "./UserList";
+// import UserList from "./UserList";
 
-const Circle = styled.div`
-  width: 5rem;
-  height: 5rem;
-  background-color: ${props => props.color || 'black'};
-  border-radius: 50%;
+const AppBlock = styled.div`
+  width: 512px;
+  margin: 0 auto;
+  margin-top: 4rem;
+  border: 1px solid black;
+  padding: 1rem;
 `;
 
-const LargeCircle = styled(Circle)`
-  ${props => 
-    props.huge &&
-    css`
-      width: 10rem;
-      height: 10rem;
-  `}
+const ButtonGroup = styled.div`
+  & + & {
+    margin-top: 1rem;
+  }
 `;
 
 function App() {
   return (
-    <div>
-      <Circle color="blue"></Circle>
-      <LargeCircle color="red" huge></LargeCircle>
-    </div>
+    <ThemeProvider
+      theme={{
+        palette: {
+          blue: '#228be6',
+          gray: '#495057',
+          pink: '#f06595'
+        }
+      }}
+    >
+
+      <AppBlock>
+        <ButtonGroup>
+          <Button size="large">BUTTON</Button>
+          <Button>BUTTON</Button>
+          <Button size="small">BUTTON</Button>
+        </ButtonGroup>
+
+        <ButtonGroup>
+          <Button color="gray" size="large">BUTTON</Button>
+          <Button color="gray">BUTTON</Button>
+          <Button color="gray" size="small">BUTTON</Button>
+        </ButtonGroup>
+
+        <ButtonGroup>
+          <Button color="pink" size="large">BUTTON</Button>
+          <Button color="pink">BUTTON</Button>
+          <Button color="pink" size="small">BUTTON</Button>
+        </ButtonGroup>
+
+        <ButtonGroup>
+          <Button size="large" outline>BUTTON</Button>
+          <Button color="gray" outline>BUTTON</Button>
+          <Button color="pink" size="small" outline>BUTTON</Button>
+        </ButtonGroup>
+
+        <ButtonGroup>
+          <Button size="large" fullWidth>
+            BUTTON
+          </Button>
+          <Button size="large" color="gray" fullWidth>
+            BUTTON
+          </Button>
+          <Button size="large" color="pink" fullWidth>
+            BUTTON
+          </Button>
+        </ButtonGroup>
+      </AppBlock>
+
+    </ThemeProvider>
   )
 }
 
