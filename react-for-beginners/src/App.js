bec48302ea7b2cdbca6f2945ad725790d5e4b5b5
yunbiyomi <yunbiyomi@naver.com>
2023-06-05 23:02:41 +0900

    20230605

---
+++ react-for-beginners/src/App.js
@@ -0,0 +1,12 @@
+import Button from "./Button";
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome back!!!</h1>
+      <Button text={"Continue"}/>
+    </div>
+  );
+}
+
+export default App;
