a49b21627f9be9e601e71cf21fb5d6eb9dce4f9b
yunbiyomi <yunbiyomi@naver.com>
2023-06-07 22:37:52 +0900

    20230607

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,10 +1,20 @@
+import { useEffect, useState } from "react";
 import Button from "./Button";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, SetKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => SetKeyword(event.target.value);
+  console.log("i run all the time");
+  useEffect(() => {
+    console.log("CALL THE API.....");
+  }, []);
   return (
     <div>
-      <h1>Welcome back!!!</h1>
-      <Button text={"Continue"}/>
+      <input value={keyword} onChange={onChange} type="text" placeholder="Search"></input>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
